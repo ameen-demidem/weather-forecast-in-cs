fc3ec5e71b5890e6a0d891a638ae7049be551701
Ameen Demidem <ameen.demidem@gmail.com>
2016-12-26 09:12:06 -0500

    Re-styling only

--- Program.cs
+++ Program.cs
@@ -8,24 +8,22 @@ using Microsoft.Extensions.Configuration;
 
 namespace WeatherMicroservice
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-           var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
+  public class Program {
+    public static void Main(string[] args) {
+      var config = new ConfigurationBuilder()
+        .AddCommandLine(args)
+        .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+        .Build();
 
-            var host = new WebHostBuilder()
-                .UseConfiguration(config)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+      var host = new WebHostBuilder()
+        .UseConfiguration(config)
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-            host.Run();
-        }
+      host.Run();
     }
+  }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,30 +8,30 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace WeatherMicroservice
-{
-    public class Startup
+namespace WeatherMicroservice {
+  public class Startup {
+    // This method gets called by the runtime.
+    // Use this method to add services to the container.
+    // For more information on how to configure your application,
+    // visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services) { }
+
+    // This method gets called by the runtime.
+    // Use this method to configure the HTTP request pipeline.
+    public void Configure(
+        IApplicationBuilder app,
+        IHostingEnvironment env,
+        ILoggerFactory loggerFactory)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+      loggerFactory.AddConsole();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
+      if (env.IsDevelopment()) {
+        app.UseDeveloperExceptionPage();
+      }
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+      app.Run(async (context) => {
+        await context.Response.WriteAsync("Hello World!");
+      });
     }
+  }
 }
