bf142a9fda5a974a36fdb48800ebb716d6639776
Ameen Demidem <ameen.demidem@gmail.com>
2016-12-26 11:54:01 -0500

    Add a random weather forecast

--- Startup.cs
+++ Startup.cs
@@ -19,9 +19,9 @@ namespace WeatherMicroservice {
     // This method gets called by the runtime.
     // Use this method to configure the HTTP request pipeline.
     public void Configure(
-        IApplicationBuilder app,
-        IHostingEnvironment env,
-        ILoggerFactory loggerFactory)
+      IApplicationBuilder app,
+      IHostingEnvironment env,
+      ILoggerFactory loggerFactory)
     {
       loggerFactory.AddConsole();
 
@@ -32,9 +32,27 @@ namespace WeatherMicroservice {
       app.Run(async (context) => {
         var lat = context.Request.Query["lat"].FirstOrDefault().TryParse();
         var lng = context.Request.Query["long"].FirstOrDefault().TryParse();
-        await context.Response.WriteAsync(
-          $"Retrieving Weather for lat: {lat}, long: {lng}"
-        );
+
+        if (lat.HasValue && lng.HasValue) {
+          var forecast = new List<WeatherReport>();
+          for (var days = 1; days < 6; days++) {
+            forecast.Add(new WeatherReport(lat.Value, lng.Value, days));
+          }
+
+          await context.Response.WriteAsync(
+            $"Weather forecast for lat: {lat}, long: {lng}\n" +
+            $"  Today:\n" +
+            $"    {forecast[0].Format()}\n" +
+            $"  Tomorrow:\n" +
+            $"    {forecast[1].Format()}\n" +
+            $"  After tomorrow:\n" +
+            $"    {forecast[2].Format()}\n" +
+            $"  Three days from now:\n" +
+            $"    {forecast[3].Format()}\n" +
+            $"  Four days from now:\n" +
+            $"    {forecast[4].Format()}\n"
+          );
+        }
       });
     }
   }
@@ -47,6 +65,15 @@ namespace WeatherMicroservice {
       } else {
         return default(double?);
       }
+    }
+
+    public static string Format(this WeatherReport report) {
+      return (
+        $"General condition: {report.Conditions}, " +
+        $"Highest Temperature: {report.HiTemperature}, " +
+        $"Lowest Temperature: {report.LoTemperature}, " +
+        $"Average Wind Speed: {report.AverageWindSpeed}."
+      );
     }
   }
 }
